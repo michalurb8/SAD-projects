6ba1ccedeac3eb84da19b19e18e67951bcb5b606
oleh10hoba <56839343+oleh10hoba@users.noreply.github.com>
2022-05-12 13:09:25 +0200

    Create zad3.R

---
+++ proj1/zad3.R
@@ -0,0 +1,9 @@
+katastrofy <- read.csv("/Users/user/Downloads/katastrofy.csv")
+
+antonov <- subset(katastrofy, substr(katastrofy$Type,1,7) == "Antonov")
+liczba_antonov = length(antonov$Date)
+liczba_wszystkich = length(katastrofy$Date)
+
+slices <- c(liczba_antonov, liczba_wszystkich)
+lbls <- c("Antonov", "Reszta")
+pie(slices, labels = lbls, main="Pie Katastrofy z udziałem samolotów Antonov")
