38ae619b0565b289251d28a8da55bf728f14de09
oleh10hoba <56839343+oleh10hoba@users.noreply.github.com>
2022-05-12 13:15:38 +0200

    Update zad3.R

--- proj1/zad3/zad3.R
+++ proj1/zad3/zad3.R
@@ -3,6 +3,7 @@ katastrofy <- read.csv("/Users/user/Downloads/katastrofy.csv")
 antonov <- subset(katastrofy, substr(katastrofy$Type,1,7) == "Antonov")
 liczba_antonov = length(antonov$Date)
 liczba_wszystkich = length(katastrofy$Date)
+stosunek = liczba_antonov / liczba_wszystkich
 
 slices <- c(liczba_antonov, liczba_wszystkich)
 lbls <- c("Antonov", "Reszta")
